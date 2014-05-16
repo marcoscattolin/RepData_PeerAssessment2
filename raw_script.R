3b391db8ba7b66d7342a2cb3efdab0c09e36c348
marcoscattolin <marco.scattolin@gmail.com>
2014-05-17 01:28:40 +0200

    Economic version

--- raw_script.R
+++ raw_script.R
@@ -6,17 +6,14 @@ library(maps)
 bigdata <- read.csv(bzfile("./data/repdata-data-StormData.csv.bz2"))
 
 #select relevant columns
-data <- bigdata[,c("STATE","COUNTYNAME","EVTYPE","FATALITIES","INJURIES","PROPDMG","PROPDMGEXP","CROPDMG","CROPDMGEXP","LATITUDE","LONGITUDE")]
+data <- bigdata[,c("STATE","EVTYPE","FATALITIES","INJURIES","PROPDMG","PROPDMGEXP","CROPDMG","CROPDMGEXP","LATITUDE","LONGITUDE")]
 
 #columns to lower case
 colnames(data) <- tolower(colnames(data))
 
-#damage exp to upper case
-data$propdmgexp <- toupper(data$propdmgexp)
-data$cropdmgexp <- toupper(data$cropdmgexp)
-
-
-#evttype to upper case
+#damage and event exp to upper case
+data$propdmgexp <- factor(toupper(data$propdmgexp))
+data$cropdmgexp <- factor(toupper(data$cropdmgexp))
 data$evtype <- factor(toupper(data$evtype))
 
 
@@ -26,19 +23,18 @@ table(data$propdmgexp)
 
 
 #look for not valid values for cropdmgexp
-validexp <- c("","H","K","M","B")
-invalid.exp <- !(data$cropdmgexp %in% validexp)
+invalid.exp <- !(data$cropdmgexp %in% c("","0","H","K","M","B"))
 mean(invalid.exp)
 data[invalid.exp,]
 
-#look for not valid for cropdmgexp
-invalid.exp <- !(data$propdmgexp %in% validexp)
+#look for not valid for propdmgexp
+invalid.exp <- !(data$propdmgexp %in% c("","0","H","K","M","B"))
 mean(invalid.exp)
 data[invalid.exp,]
 
 
 #define valid multipliers
-exponents <- data.frame(c("","H","K","M","B"),c(1,10^2,10^3,10^6,10^9))
+exponents <- data.frame(c("","0","H","K","M","B"),c(1,1,10^2,10^3,10^6,10^9))
 colnames(exponents) <- c("validexp","multiplier")
 
 #subset over valid exps
@@ -63,6 +59,41 @@ data$latitude <- data$latitude/100
 data <- data[,c("state","evtype","fatalities","injuries","propdmg","cropdmg","latitude","longitude")]
 
 
+#----------economic section
+data$totalCost <- data$propdmg+data$cropdmg   #define total cost
+economicData <- subset(data, totalCost > 0)
+economicData <- economicData[,c("state","evtype","totalCost","propdmg","cropdmg","latitude","longitude")]
+economicData$evtype <- factor(economicData$evtype)
+evCosts <- tapply(economicData$totalCost,economicData$evtype,sum)
+plot(1:394,evCosts, main="Few events have very high costs")
+evCosts <- data.frame(evCosts[order(evCosts, decreasing=T)])
+colnames(evCosts) <- "totalCost"
+evCosts$magnitude <- c(rep("Major",10),rep("Minor",394-10))
+tapply(evCosts$totalCost,evCosts$magnitude,sum) #considering only top 10 events captures the most
+evCosts$evtype <- row.names(evCosts)
+evCosts <- evCosts[,c("evtype","magnitude")] 
+economicData <- merge(economicData,evCosts)
+economicData <- subset(economicData, magnitude =="Major")
+
+economicData$evtype <- factor(economicData$evtype)
+table(economicData$state,economicData$evtype)
+
+economicData <- dcast(economicData, state~evtype,fun.aggregate=sum,value.var="totalCost")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#BACKUP
 #Define economicDmgScore using PCA
 pca <- princomp(data[,c("propdmg","cropdmg")], scale=TRUE)
 summary(pca)
@@ -72,7 +103,7 @@ data[head(order(data$economicDmgScore,decreasing=T)),]
 
 
 #Define healthDmgScore using PCA
-pca <- princomp(data[,c("fatalities","injuries")], scale=TRUE)
+pca <- princomp(data[,c("fatalities","injuries")])
 summary(pca)
 plot(pca,type="lines", main = "Screeplot for Economic damage")
 data$healthDmgScore <- pca$scores[,1]        
